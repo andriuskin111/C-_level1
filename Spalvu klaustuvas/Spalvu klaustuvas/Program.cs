6bb66deb9bc29c118bd7e2e97e4b767808a857c3
andriuskin111 <andriuskin@gmail.com>
2019-12-10 21:15:47 +0200

    prideta spalvu klausimo funkcija

--- Spalvu klaustuvas/Spalvu klaustuvas/Program.cs
+++ Spalvu klaustuvas/Spalvu klaustuvas/Program.cs
@@ -9,7 +9,19 @@ namespace Spalvu_klaustuvas
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            int klausimuSkaicius = 1;
+            string spalva = "";
+            while(spalva != "exit")
+            {
+                Console.WriteLine("Klausimas Nr {0}", klausimuSkaicius);
+                Console.WriteLine("Kokia tavo megstamiausia spalva?");
+                spalva = Console.ReadLine();
+                klausimuSkaicius ++;
+                Console.WriteLine("Mano irgi:) [Baigti zaidima - exit]");
+                Console.WriteLine();
+                
+            }
         }
     }
 }
