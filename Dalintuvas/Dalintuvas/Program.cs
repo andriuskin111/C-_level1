1b550f99bc5b0cc8f741ec487242bfe209e500c5
andriuskin111 <andriuskin@gmail.com>
2019-12-10 22:05:02 +0200

    pridetas kalkuliatorius

--- Dalintuvas/Dalintuvas/Program.cs
+++ Dalintuvas/Dalintuvas/Program.cs
@@ -10,6 +10,51 @@ namespace Dalintuvas
     {
         static void Main(string[] args)
         {
+            double a;
+            double b;
+            double rezultatas;
+            string veiksmas = "";
+            while (veiksmas != "exit")
+            {
+            Console.WriteLine("iveskite pirma skaiciu");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("iveskite antra skaiciu");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("iveskite veiksma [+ - / *]");
+            veiksmas = Console.ReadLine();
+                Console.WriteLine(veiksmas);
+
+            if (veiksmas == "+")
+            {
+                rezultatas = a + b;
+                    Console.WriteLine("Resultatas yra: {0} ", rezultatas);
+                }
+
+            if (veiksmas == "-")
+            {
+                rezultatas = a - b;
+                    Console.WriteLine("Resultatas yra: {0} ", rezultatas);
+                }
+
+            if (veiksmas == "/")
+            {
+                rezultatas = a / b;
+                    Console.WriteLine("Resultatas yra: {0} ", rezultatas);
+                }
+
+            if (veiksmas == "*")
+            {
+                rezultatas = a * b;
+                    Console.WriteLine("Resultatas yra: {0} ", rezultatas);
+                }
+            else
+            {
+                Console.WriteLine("Neteisingas veiksmas, iveskite is naujo");
+            }
+                
+            veiksmas = "";
+            Console.ReadLine();
+            }
         }
     }
 }
