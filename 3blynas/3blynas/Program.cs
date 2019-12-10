c7e57d1e9dfbdf7c3ca4aff8613072e2780d0957
andriuskin111 <andriuskin@gmail.com>
2019-12-10 20:48:46 +0200

    Added Vardas

--- 3blynas/3blynas/Program.cs
+++ 3blynas/3blynas/Program.cs
@@ -10,6 +10,16 @@ namespace _3blynas
     {
         static void Main(string[] args)
         {
+            string vardas = "";
+            while (vardas != "stop")
+            {
+                Console.WriteLine("Iveskite savo varda: ");
+                vardas = Console.ReadLine();
+                Console.WriteLine("Sveiki {0} [ENTER - testi]", vardas);
+            }
+            Console.Clear();
+            Console.WriteLine("ate...");
+            Console.ReadLine();
         }
     }
 }
