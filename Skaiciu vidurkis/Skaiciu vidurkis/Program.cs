dfc653a40e962935843fc04b772c990811eaf790
andriuskin111 <andriuskin@gmail.com>
2019-12-11 22:36:08 +0200

    stabili su exceptions

--- Skaiciu vidurkis/Skaiciu vidurkis/Program.cs
+++ Skaiciu vidurkis/Skaiciu vidurkis/Program.cs
@@ -10,6 +10,38 @@ namespace Skaiciu_vidurkis
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Skaiciu vidurkio skaiciavimas");
+            Console.WriteLine("[Pradeti - Enter] [iseiti - e]");
+            while (Console.ReadLine() != "e")
+            {                
+                try
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Iveskite pirma skaiciu");
+                    double sk1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Iveskite antra skaiciu");
+                    double sk2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Iveskite trecia skaiciu");
+                    double sk3 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Iveskite ketvirta skaiciu");
+                    double sk4 = Convert.ToDouble(Console.ReadLine());
+
+                    double vidurkis = (sk1 + sk2 + sk3 + sk4) / 4;
+
+                    Console.WriteLine();
+                    Console.WriteLine("Ivestu skaiciu vidurkis yra: {0}", vidurkis);
+                    Console.WriteLine();
+                    Console.WriteLine("[Pakartoti - Enter] [iseiti - e]");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+            
         }
     }
 }
