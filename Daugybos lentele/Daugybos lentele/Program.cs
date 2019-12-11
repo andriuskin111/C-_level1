ebaf5e8174d73efe41bab963200cd4d6a8fbcb54
andriuskin111 <andriuskin@gmail.com>
2019-12-11 21:48:01 +0200

    Daugybos_lentele_start

--- Daugybos lentele/Daugybos lentele/Program.cs
+++ Daugybos lentele/Daugybos lentele/Program.cs
@@ -10,6 +10,82 @@ namespace Daugybos_lentele
     {
         static void Main(string[] args)
         {
+            int skaicius;
+            int rezultatas;
+            string klaviatura = "";
+
+            while (klaviatura != "exit")
+            {
+                Console.WriteLine();
+                Console.WriteLine("iveskite sveika skaiciu nuo 1 iki 10 [Iseiti - exit]");
+                klaviatura = Console.ReadLine();
+
+                switch (klaviatura)
+                {
+                    case "1":
+                        skaicius = Convert.ToInt32(klaviatura);
+                        IsvestiLentele(skaicius);
+                        break;
+                    case "2":
+                        skaicius = Convert.ToInt32(klaviatura);
+                        IsvestiLentele(skaicius);
+                        break;
+                    case "3":
+                        skaicius = Convert.ToInt32(klaviatura);
+                        IsvestiLentele(skaicius);
+                        break;
+                    case "4":
+                        skaicius = Convert.ToInt32(klaviatura);
+                        IsvestiLentele(skaicius);
+                        break;
+                    case "5":
+                        skaicius = Convert.ToInt32(klaviatura);
+                        IsvestiLentele(skaicius);
+                        break;
+                    case "6":
+                        skaicius = Convert.ToInt32(klaviatura);
+                        IsvestiLentele(skaicius);
+                        break;
+                    case "7":
+                        skaicius = Convert.ToInt32(klaviatura);
+                        IsvestiLentele(skaicius);
+                        break;
+                    case "8":
+                        skaicius = Convert.ToInt32(klaviatura);
+                        IsvestiLentele(skaicius);
+                        break;
+                    case "9":
+                        skaicius = Convert.ToInt32(klaviatura);
+                        IsvestiLentele(skaicius);
+                        break;
+                    case "10":
+                        skaicius = Convert.ToInt32(klaviatura);
+                        IsvestiLentele(skaicius);
+                        break;
+                    case "exit":
+                        Console.WriteLine("Viso geriausio!");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Netesingas skaicius, bandykite dar karta");
+                        break;
+                }
+                   
+                //skaicius = Convert.ToInt32(Console.ReadLine());
+            }
+
+            void IsvestiLentele(int aSkaicius)
+            {
+                Console.WriteLine("Ivesto skaiciaus daugybos lentele:");
+
+                for (int i = 1; i < 11; i++)
+                {
+                    rezultatas = aSkaicius * i;
+                    Console.WriteLine(skaicius + " x " + i + " = " + rezultatas);
+                }
+            }
+
+
         }
     }
 }
